b6e37babab3bfe11cdb27ca28e401c96e742ecd0
Hashimrasheed <hashimrasheed1999@gmail.com>
2022-02-23 22:21:00 +0530

    cart api s started

--- api/controllers/CartController.js
+++ api/controllers/CartController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { JWT_KEY } = require("../config/constant");
 const mongoose = require("mongoose")
+const _ = require("lodash")
 
 exports.fetchCart = async (req, res, next) => {
   try {
@@ -104,12 +105,15 @@ exports.fetchCart = async (req, res, next) => {
 
 exports.addToCart = async (req, res) => {
   try {
-    let {customerType, userId} = req.body
+    let { customerType, userId } = req.body
     let cartData = await Cart
-        .findOne({ customerId: userId, customerType: customerType })
-        .lean()
-        .exec();
-    
+      .findOne({
+        customerId: userId,
+        customerType: customerType
+      })
+      .lean()
+      .exec();
+
     let product = {
       product_id: req.body.product.product_id,
       selection_type: req.body.product.selection_type,
@@ -117,58 +121,58 @@ exports.addToCart = async (req, res) => {
     }
     if (cartData && cartData.cartProducts && cartData.cartProducts[0]) {
       let haveSameProduct = false
-      let newCart = []
-        await cartData.cartProducts.forEach(async (cartProduct) => {
-            let cartProductData = {
-              product_id: String(cartProduct.product_id),
-              selection_type: cartProduct.selection_type,
-              selection_price_id: cartProduct.selection_price_id ? String(cartProduct.selection_price_id) : null,
-            }
-            if (_.isEqual(product, cartProductData)) {
-              haveSameProduct = true
-              await Cart.updateOne(
-                {
-                  customerId: userId,
-                  "cartProducts._id": { $in: cartProduct._id },
-                }, {
-                  $inc: {"cartProducts.quantity": req.body.product.quantity}
-                }
-                )
-            } 
-        })
+      await cartData.cartProducts.forEach(async (cartProduct) => {
+        let cartProductData = {
+          product_id: String(cartProduct.product_id),
+          selection_type: cartProduct.selection_type,
+          selection_price_id: cartProduct.selection_price_id ? String(cartProduct.selection_price_id) : null,
+        }
+        console.log(product, cartProductData)
+        if (_.isEqual(product, cartProductData)) {
+          haveSameProduct = true
+          await Cart.updateOne(
+            {
+              customerId: userId,
+              "cartProducts._id": { $in: cartProduct._id },
+            }, {
+            $inc: { "cartProducts.$.quantity": req.body.product.quantity }
+          }
+          )
+        }
+      })
       if (!haveSameProduct) {
-        product.quantity= req.body.product.quantity
+        product.quantity = req.body.product.quantity
         await Cart.updateOne(
           {
             customerId: userId,
           }, {
-            $push: { cartProducts: product}
-          }
-          )
+          $push: { cartProducts: product }
+        }
+        )
       }
       return res.status(200).json({
         data: {
           product: req.body.product.product_id
         },
         status: 200,
-        message: "Product updated successfully",
+        message: "Product added to cart",
       });
     } else if (cartData) {
-      product.quantity= req.body.product.quantity
-        await Cart.updateOne(
-          { customerId: userId },
-          { $push: { cartProducts: product } }
-        )
-          .exec();
+      product.quantity = req.body.product.quantity
+      await Cart.updateOne(
+        { customerId: userId },
+        { $push: { cartProducts: product } }
+      )
+        .exec();
       return res.status(200).json({
         data: {
           product: req.body.product.product_id
         },
         status: 200,
-        message: "Product updated successfully",
+        message: "Product added to cart",
       });
     } else {
-      product.quantity= req.body.product.quantity
+      product.quantity = req.body.product.quantity
       let newCustomerCart = {
         _id: new mongoose.Types.ObjectId,
         business_slug: req.body.business_slug,
@@ -186,6 +190,55 @@ exports.addToCart = async (req, res) => {
         message: "Product updated successfully",
       });
     }
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message,
+      status: 400,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.changeQuantity = async (req, res) => {
+  try {
+    let { customerType, userId, cartProductId, quantity } = req.body
+
+    await Cart.updateOne(
+      {
+        customerId: userId,
+        "cartProducts._id": { $in: cartProductId },
+      }, {
+      $set: { "cartProducts.$.quantity": quantity }
+    }
+    )
+    return res.status(200).json({
+      status: 200,
+      message: "Quantity updated successfully",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message,
+      status: 400,
+      message: "Something went wrong",
+    });
+  }
+};
+
+exports.removeProduct = async (req, res) => {
+  try {
+    let { customerType, userId, cartProductId } = req.body
+
+    await Cart.updateOne(
+      {
+        customerId: userId,
+      }, {
+      $pull: { "cartProducts": {_id: cartProductId }}
+    }
+    )
+    return res.status(200).json({
+      status: 200,
+      message: "Product removed successfully",
+    });
   } catch (error) {
     return res.status(400).json({
       error: error.message,
--- api/routes/cart.js
+++ api/routes/cart.js
@@ -4,8 +4,10 @@ var check_auth = require('../middleware/check-auth');
 var CartController = require('../controllers/CartController');
 
 
-router.get('/', check_auth, CartController.fetchCart);
-router.get('/', check_auth, CartController.addToCart);
+router.get('/', CartController.fetchCart);
+router.post('/addToCart', CartController.addToCart);
+router.post('/changeQuantity', CartController.changeQuantity);
+router.post('/removeProduct', CartController.removeProduct);
 
 
 module.exports = router;
