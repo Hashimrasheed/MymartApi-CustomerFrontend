dee0e5477046b3716d2879174ef3835eea7e1cb6
Hashim <emst.hashim@gmail.com>
2022-03-19 16:04:01 +0530

    cart modal name capitalized

--- api/config/constant.js
+++ api/config/constant.js
@@ -1,26 +1,18 @@
-//production
-// module.exports = Object.freeze({
-//     Appurl: 'https://foodselleradmin.joinviral.com',
-//     ImageUrl: 'https://foodselleradmin.joinviral.com',
-//     ProdResetPass: 'https://joinviral.com/seller/#/reset-password/',
-//     JWT_KEY: "topsecretsupersecretsecret",
-//     primarydomain: 'viralshop.live',
-// });
 
 //Development
-// module.exports = Object.freeze({
-//     Appurl: 'http://3.23.223.86:3002',
-//     ImageUrl: 'http://3.23.223.86:3002',
-//     ProdResetPass: 'http://3.23.223.86/seller/#/reset-password/',
-//     JWT_KEY: "topsecretsupersecretsecret",
-//     primarydomain: 'dev.viralshop.live',
-// })
+module.exports = Object.freeze({
+    Appurl: 'http://54.167.53.18:9002',
+    ImageUrl: 'http://54.167.53.18:9002',
+    ProdResetPass: 'http://54.167.53.18/seller/#/reset-password/',
+    JWT_KEY: "topsecretsupersecretsecret",
+    primarydomain: 'dev.mymart.live',
+})
 
 //Local
-module.exports = Object.freeze({
-    Appurl: 'http://localhost:3002',
-    ImageUrl: "http://localhost:3002",
-    ProdResetPass: 'http://localhost/seller/#/reset-password/',
-    JWT_KEY: "topsecretsupersecretsecret",
-    primarydomain: 'dev.viralshop.live',
-})
+// module.exports = Object.freeze({
+//     Appurl: 'http://localhost:9002',
+//     ImageUrl: "http://localhost:9002",
+//     ProdResetPass: 'http://localhost/seller/#/reset-password/',
+//     JWT_KEY: "topsecretsupersecretsecret",
+//     primarydomain: 'dev.mymart.live',
+// })
